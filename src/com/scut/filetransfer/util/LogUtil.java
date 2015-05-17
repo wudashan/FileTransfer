77987f8a770447ae6778a33a82aa836508f77461
wudashan <453685754@qq.com>
2015-05-17 18:31:21 +0800

    gg

---
+++ src/com/scut/filetransfer/util/LogUtil.java
@@ -0,0 +1,40 @@
+package com.scut.filetransfer.util;
+
+import android.util.Log;
+
+public class LogUtil {
+		private static final int VERBOSE = 1;
+		private static final int DEBUG =2;
+		private static final int INFO = 3;
+		private static final int WARN = 4;
+		private static final int ERROR = 5;
+		//发布版本时修改LEVEL的值大于5即可
+		private static final int  LEVEL = VERBOSE;
+		
+		public static void v(String tag,String msg){
+			if (LEVEL <= VERBOSE) {
+				Log.v(tag, msg);
+			}
+		}
+		
+		public static void d(String tag,String msg){
+			if (LEVEL <= DEBUG) {
+				Log.d(tag, msg);
+			}
+		}
+		public static void i(String tag,String msg){
+			if (LEVEL <= INFO) {
+				Log.i(tag, msg);
+			}
+		}
+		public static void w(String tag,String msg){
+			if (LEVEL <= WARN) {
+				Log.w(tag, msg);
+			}
+		}
+		public static void e(String tag,String msg){
+			if (LEVEL <= ERROR) {
+				Log.e(tag, msg);
+			}
+		}
+}
